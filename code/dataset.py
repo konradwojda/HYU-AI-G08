cf53bd51d64719de616b9c2dc56dab3eb2bab6d6
Konrad Wojda <konrad2001@outlook.com>
2024-10-10 17:53:05 +0900

    Add DeepFakeDataset class

---
+++ code/dataset.py
@@ -0,0 +1,29 @@
+import os
+from torch.utils.data import Dataset
+from PIL import Image
+
+
+class DeepFakeDataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+        self.image_paths = []
+        self.labels = []
+        for label, class_name in enumerate(["real", "fake"]):
+            class_dir = os.path.join(root_dir, class_name)
+            for image_name in os.listdir(class_dir):
+                self.image_paths.append(os.path.join(class_dir, image_name))
+                self.labels.append(label)  # 0 for real, 1 for fake
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        image_path = self.image_paths[idx]
+        image = Image.open(image_path).convert("RGB")
+        label = self.labels[idx]
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, label
